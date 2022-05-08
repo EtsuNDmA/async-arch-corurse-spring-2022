fa50e8ec11cb13fb23177fa3d9fa0e7a1ebd1bd0
Dmitry Nikolaev <etsu.nikolaev@gmail.com>
2022-05-08 19:23:46 +0300

    wip

---
+++ auth/app/schemas.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+from pydantic import BaseModel, EmailStr
+
+from app.db.models import Role
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    role: str = Role.ADMIN
+
+
+class UserRead(BaseModel):
+    username: str
+    is_active: bool
+    email: EmailStr
+    role: str
+
+    class Config:
+        orm_mode = True
